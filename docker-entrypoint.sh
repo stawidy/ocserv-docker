5fdee15165b166d7af811ebcd6c88f6442246da8
stawidy <duyizhaozj321@yahoo.com>
2017-02-19 20:54:52 +0800

    Update iptables rules

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -44,6 +44,7 @@ sysctl -w net.ipv4.ip_forward=1
 
 # Enable NAT forwarding
 iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 
 # Enable TUN device
 mkdir -p /dev/net
